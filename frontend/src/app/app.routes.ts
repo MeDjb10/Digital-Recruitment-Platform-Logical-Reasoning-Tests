31dbd44a2534de8aa92b2c2dae892f9be71b1557
MeDjb10 <jabou.medamine@gmail.com>
2025-03-11 13:39:59 +0100

    Refactor routing and enhance Forgot Password UI: Remove unused imports, update background gradient, and improve form styling

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,4 @@
 import { Routes } from '@angular/router';
-import { AuthLayoutComponent } from './features/auth/components/auth-layout/auth-layout.component';
-import { LoginComponent } from './features/auth/pages/login/login.component';
-import { SignupComponent } from './features/auth/pages/signup/signup.component';
-
-import { HomeComponent } from './features/homepage/components/home/home.component';
-import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard.component';
-import { GeneralStatsComponent } from './features/dashboard/components/general-stats/general-stats.component';
-import { UsersListComponent } from './features/dashboard/components/users-list/users-list.component';
-import { StatsRLComponent } from './features/dashboard/components/stats-rl/stats-rl.component';
-import { UsersListRLComponent } from './features/dashboard/components/users-list-rl/users-list-rl.component';
-import { TestsListRLComponent } from './features/dashboard/components/tests-list-rl/tests-list-rl.component';
 
 
 export const routes: Routes = [
