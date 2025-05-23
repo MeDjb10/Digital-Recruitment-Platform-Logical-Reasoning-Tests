3dfbf44cfdc10d58f5f036db62238262d51b6c3a
Mohamed Amine Jabou <jabou.medamine@gmail.com>
2025-05-23 13:10:40 +0100

    Fix paths in startall.bat for backend services

--- startall.bat
+++ startall.bat
@@ -5,16 +5,22 @@ REM Start Frontend
 start "Frontend" cmd /k "cd frontend && ng s --o"
 
 REM Start API Gateway
-start "API Gateway" cmd /k "cd api-gateway && npm run dev"
+start "API Gateway" cmd /k "cd backend/api-gateway && npm run dev"
 
 REM Start Authentication Service
-start "Auth Service" cmd /k "cd authentification-service && npm run dev"
+start "Auth Service" cmd /k "cd backend/authentification-service && npm run dev"
 
 REM Start Test Service
-start "Test Service" cmd /k "cd test-service && npm run dev"
+start "Test Service" cmd /k "cd backend/test-service && npm run dev"
 
 REM Start User Management Service
-start "User Service" cmd /k "cd user-management-service && npm run dev"
+start "User Service" cmd /k "cd backend/user-management-service && npm run dev"
+
+REM Start Test Assignment Service
+start "Test Assignement Service" cmd /k "cd backend/test-assignment-service && npm run dev"
+
+REM Start Notification Service
+start "Notification Service" cmd /k "cd backend/notification-service && npm run dev"
 
 REM Start AI Classification
 start "AI Classification" cmd /k "cd ai-classification\ml && venv\Scripts\activate && cd src\api && python server.py"
